4205435b98de89c830d5bc8f44bf9ee1e43538a7
fattah <m.agahi@outlook.com>
2020-09-19 13:27:58 +0430

    as

--- bashtestjson.sh
+++ bashtestjson.sh
@@ -6,13 +6,18 @@ username="monitor"
 token="119cf029e9a6e0d1ed8445137a6f17d658"
 url="http://$username:$token@$server"
 jq='jq-linux64'
-> /tmp/api2.json
-> /tmp/jobnames.txt
+> /tmp/jenkins/api/json
+> /tmp/jenkins/jobnames.txt
 > /tmp/api1.json
 
 function get_job_names {
-	curl --silent --show-error $url/api/json > /tmp/api1.json
-	./$jq '.jobs[].name' /tmp/api1.json | sed -e 's/^"\|"$//g' > /tmp/jobnames.txt
+	curl --silent --show-error $url/api/json > /tmp/jenkins/api/json
+	./$jq '.jobs[].name' /tmp/jenkins/api/json | sed -e 's/^"\|"$//g' > /tmp/jenkins/jobnames.txt
+}
+
+function get_job_index {
+	search_string="$1"
+
 }
 
 function get_job_descriptions {
@@ -35,13 +40,19 @@ function get_job_success_rate {
 		then
 			continue
 		fi
-		echo $line | ./$jq '.healthReport[].score' 
+		echo $line
+		echo $1
+		if $line -eq $1
+		then
+			search_string=".healthReport[$1].score"
+			echo $line | ./$jq $search_string
+		fi
 	done < /tmp/api2.json 	
 }
 
 function get_job_current_status {
-
-	status=`./$jq '.jobs[$1].color' /tmp/api1.json | sed -e 's/^"\|"$//g'`
+	search_string=".jobs[$1].color"
+	status=`./$jq $search_string /tmp/api1.json | sed -e 's/^"\|"$//g'`
 	echo $status	
 
 }
@@ -49,5 +60,5 @@ function get_job_current_status {
 
 get_job_names
 get_job_descriptions
-get_job_success_rate
-get_job_current_status 0
+get_job_success_rate 0
+get_job_current_status 0 
