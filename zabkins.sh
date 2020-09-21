a48f6ef0dc23e32b88182942d7acacc996343443
fattah <m.agahi@outlook.com>
2020-09-21 22:29:37 +0330

    zabbix send 1

--- zabkins.sh
+++ zabkins.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ### initial variables
+agenthost="`hostname -f`"
+zserver="192.168.145.209"
+zport="10051"
+
 server="192.168.145.173:8080"
 username="monitor"
 token="119cf029e9a6e0d1ed8445137a6f17d658"
@@ -11,9 +15,12 @@ jenkins_jobnames_file="/etc/zabbix/tmp/jenkins_jobnames.txt"
 jenkins_job_api_file="/etc/zabbix/tmp/jenkins_job_api.json"
 jenkins_return_file="/etc/zabbix/tmp/jenkins_return.txt"
 jenkins_job_items="success_rate"
+jenkins_stats_file="/etc/zabbix/tmp/jenkins_stats.txt"
+zabbix_sender_log_file="/etc/zabbix/tmp/zsender.log"
 > $jenkins_api_file
 > $jenkins_jobnames_file
 > $jenkins_job_api_file
+> $jenkins_stats_file
 
 function get_job_count {
 	curl --silent --show-error $url/api/json > $jenkins_api_file
@@ -66,6 +73,17 @@ function get_job_current_status {
 
 }
 
+function send_stats {
+	while IFS= read -r line2
+	do
+		for item in $jenkins_job_items
+		do
+			echo $agenthost jenkins.job["$item"] 200 >> $jenkins_stats_file
+		done
+	done
+	zabbix_sender -vv -z $zserver -p $zport -i $jenkins_stats_file >> $zabbix_sender_log_file 2>&1
+}
+
 if  [ $1 = 'jobcount' ] 
 then
 	get_job_count
@@ -74,19 +92,6 @@ then
 	get_job_names
 	return_job_names
 	cat $jenkins_return_file
+	send_stats
 fi
-
-
-#agenthost="`hostname -f`"
-#zserver="myzabbix"
-#zport="10051"
-
-#cat /dev/null > /tmp/zdata.txt
-#for item in "carrot" "banana" "lettuce" "tomato"; do
-# randNum="$(( (RANDOM % 30)+1 ))"
-# echo $agenthost instock[$item] $randNum >> /tmp/zdata.txt
-#done
-
-# push all these trapper values back to zabbix
-#zabbix_sender -vv -z $zserver -p $zport -i /tmp/zdata.txt >> /tmp/zsender.log 2>&1
 
