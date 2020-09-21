d56d353604db124c76995bf61dc4b286b4cb855d
fattah <m.agahi@outlook.com>
2020-09-21 18:33:16 +0330

    jajs

--- zabkins.sh
+++ zabkins.sh
@@ -9,20 +9,17 @@ jq='/etc/zabbix/scripts/jq-linux64'
 jenkins_api_file="/etc/zabbix/tmp/jenkins_api.json"
 jenkins_jobnames_file="/etc/zabbix/tmp/jenkins_jobnames.txt"
 jenkins_job_api_file="/etc/zabbix/tmp/jenkins_job_api.json"
-jenkins_return_file="/etc/zabbix/tmp/jenkins_return_file.txt"
+jenkins_return_file="/etc/zabbix/tmp/jenkins_return.txt"
 
 > $jenkins_api_file
 > $jenkins_jobnames_file
 > $jenkins_job_api_file
-
-
 
 function get_job_count {
 	curl --silent --show-error $url/api/json > $jenkins_api_file
 	job_count=`$jq '.jobs[].name' $jenkins_api_file | sed -e 's/^"\|"$//g' | wc -l`
 	echo $job_count
 }
-
 
 function get_job_names {
 	curl --silent --show-error $url/api/json > $jenkins_api_file
@@ -34,48 +31,26 @@ function return_job_names {
 	echo  -n '{ "data": [' > $jenkins_return_file
 	while IFS= read -r line
 	do
-		echo -ne '\n { "{#JOBNAME}": "'$line'" },' >> $jenkins_return_file
-		
+		get_job_descriptions $line		
+		echo -ne '\n { "{#JOBNAME}": "'$line','$jenkins_job_success_rate'" },' >> $jenkins_return_file
 	done < $jenkins_jobnames_file
 	echo -e ']}' >> $jenkins_return_file
 	sed -i -e 's|\,\]\}|\n\]\}|' $jenkins_return_file
-	cat $jenkins_return_file
+	#cat $jenkins_return_file
 }
-
-
-function get_job_index {
-	search_string="$1"
-}
-
 
 function get_job_descriptions {
-	while IFS= read -r line 
-	do
-		echo  -e "#!#" >> $jenkins_job_api_file
-		line=`echo $line | sed 's| |\%20|g'`
-		curl --silent --show-error $url/job/$line/api/json >> $jenkins_job_api_file
-		echo  >> $jenkins_job_api_file
-	done < $jenkins_jobnames_file
+	> $jenkins_job_api_file
+	curl --silent --show-error $url/job/$1/api/json > $jenkins_job_api_file
+	echo  >> $jenkins_job_api_file
+	get_job_success_rate
 }
-
 
 function get_job_success_rate {
 	while IFS= read -r line
 	do
-		if echo $line | grep -qe "^#.*" 
-		then
-			continue
-		elif echo $line | grep -qe "^$"
-		then
-			continue
-		fi
-		echo $line
-		echo $1
-		if [[ $line -eq $1 ]]
-		then
-			search_string=".healthReport[$1].score"
-			echo $line | ./$jq $search_string
-		fi
+		search_string=".healthReport[].score"
+		jenkins_job_success_rate=`echo $line | ./$jq $search_string`
 	done < $jenkins_job_api_file 	
 }
 
