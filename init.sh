0cf9ca2466a410f81f03ee41f7b6870272f3aa10
fattah <m.agahi@outlook.com>
2020-09-21 11:38:04 +0330

    init.sh

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -d /etc/zabbix ]
+then 
+    if [ ! -d /etc/zabbix/tmp ]
+    then 
+        mkdir -v /etc/zabbix/tmp
+        chmod -v 777 /etc/zabbix/tmp 
+    fi
+    if [ ! -d /etc/zabbix/scripts ]
+    then
+        mkdir -v /etc/zabbix/scripts
+        chmod -v 777 /etc/zabbix/scripts 
+    fi
+
+    cp -v -f ./zabkins.conf /etc/zabbix/zabbix_agentd.d/
+    cp -v -f ./zabkins.sh /etc/zabbix/scripts/
+    cp -v -f ./jq-linux64 /etc/zabbix/scripts/
+fi
+
